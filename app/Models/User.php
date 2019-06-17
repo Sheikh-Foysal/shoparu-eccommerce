ea997a1d7b3962f315a1c08dfc0bdf62e7beaaf9
Sheikh-Foysal <openoway@gmail.com>
2019-06-17 22:04:58 +0600

    User model updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,8 +15,8 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $guraded = [
+        'remember_token'
     ];
 
     /**
