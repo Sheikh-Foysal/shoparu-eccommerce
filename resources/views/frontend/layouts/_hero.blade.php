aa357ca2c0857d467713b770ed34b45a3fbb4ca2
Sheikh <openoway@gmail.com>
2019-06-20 17:31:08 +0600

    home view done

---
+++ resources/views/frontend/layouts/_hero.blade.php
@@ -0,0 +1,10 @@
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">{{ config('app.name') }}</h1>
+            <p class="lead text-muted">Buy Anything</p>
+            <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+            </p>
+        </div>
+    </section>
